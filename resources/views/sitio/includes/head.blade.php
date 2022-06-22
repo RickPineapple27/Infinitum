57228a42b33a9cd517f8460b8a71bfa5523b75ae
Ricardo Daniel Ramirez Rivera <ricardoramz@MacBook-Pro-de-Ricardo-2.local>
2022-06-22 14:01:26 -0500

    cambio de icono

--- resources/views/sitio/includes/head.blade.php
+++ resources/views/sitio/includes/head.blade.php
@@ -2,11 +2,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Asistente infinitum cosamaloapan</title>
+    <title>Infinitum Cosamaloapan</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.svg" />
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+<link rel="icon" href="{{ url('css/favicon.ico') }}">
+<link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
     <!-- Google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
